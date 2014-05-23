be363c6d089e39062e6aae92eb2c58b1534a6aba
K Kollmann <git@k.kollmann.moe>
2014-05-23 14:01:09 +0200

    instructions rectangle excercise

---
+++ exc_01.cpp
@@ -0,0 +1,10 @@
+/*  CPP excercises
+
+    https://en.wikibooks.org/wiki/C%2B%2B_Programming/
+    Exercises/Variables_and_types
+
+    Write a program that asks the user to type the width and the
+    height of a rectangle and then outputs to the screen the area
+    and the perimeter of that rectangle.
+*/
+
