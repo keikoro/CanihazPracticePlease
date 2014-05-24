518b7caa279e9bf95c50dfea2abf7e3c87465d62
K Kollmann <git@k.kollmann.moe>
2014-05-24 14:29:59 +0200

    excercise 01a

---
+++ exc_01a.cpp
@@ -0,0 +1,30 @@
+/*  CPP excercises - 01a
+    Continuation of excercise 01.
+
+    Ex 01a
+    Write a function for inputting the width and height of the rectangle.
+
+    Ex 01
+    Write a program that asks the user to type the width and the
+    height of a rectangle and then outputs to the screen the area
+    and the perimeter of that rectangle.
+    https://en.wikibooks.org/wiki/C%2B%2B_Programming/
+    Exercises/Variables_and_types
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Please input the width and height of a rectangle:"
+    << std::endl;
+
+    int w = 0, h = 0;
+    std::cin >> w >> h;
+
+    int area = w * h;
+    int perimeter = 2*w + 2*h;
+    std::cout << "The rectangle's area is " << area << " and its "
+    "perimeter is " << perimeter << "!" << std::endl;
+
+    return 0;
+}
