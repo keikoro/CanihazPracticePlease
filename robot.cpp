44997c529f3589ae02f435d469bf9bb035b0fab7
K Kollmann <git@k.kollmann.moe>
2014-10-06 02:20:00 +0200

    add excercise robot

---
+++ robot.cpp
@@ -0,0 +1,9 @@
+/*  CPP excercises
+    Robot
+
+    Implement a class Machine with one method beep().
+    beep() is abstract.
+    Class Robot is a subclass of Machine.
+    When the Robot beeps, it puts out "*beeeeep*".
+*/
+
