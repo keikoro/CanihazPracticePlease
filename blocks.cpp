5434bda6a8f9d1b43e6011508da6f3b3cb5de689
K Kollmann <git@k.kollmann.moe>
2014-10-07 00:36:27 +0200

    create main for blocks program

--- blocks.cpp
+++ blocks.cpp
@@ -4,10 +4,22 @@
     Class Block.
     Implement all functions (constructor, dist)
     Calculate the distance between 2 blocks
-    Distance is cum of streets and avenues
+    Distance is sum of streets and avenues
 
     e.g.
     Block 1: 5th street, 3rd avenue
     Block 2: 11th street, 4th avenue
 */
 
+#include "blocks.h"
+#include <iostream>
+using namespace std;
+
+int main()  {
+    Block *firstblock = new Block(5,3);
+    Block *secondblock = new Block(11,4);
+
+    cout << firstblock->dist(secondblock) << endl;
+
+    return 0;
+}
