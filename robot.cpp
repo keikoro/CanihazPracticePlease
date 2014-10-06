63bbdd51821963464f6e128b5c294281f434bffc
K Kollmann <git@k.kollmann.moe>
2014-10-06 09:30:33 +0200

    robot program

--- robot.cpp
+++ robot.cpp
@@ -7,3 +7,24 @@
     When the Robot beeps, it puts out "*beeeeep*".
 */
 
+#include <iostream>
+using namespace std;
+
+class Machine {
+    public:
+        virtual void beep();
+};
+
+class Robot : public Machine {
+    public:
+        void beep() {
+            cout << "*beeeeep*" << endl;
+        }
+};
+
+int main () {
+    Machine *myrobot = new Robot();
+    myrobot->beep();
+
+    return 0;
+}
